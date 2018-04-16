2d663bc7f6da5e8c4c250a31ebab3bfbb557ea03
chenyang <chenyang@qq.com>
2018-04-16 13:25:06 +0800

    获取AccessToken并缓存，缓存使用Doctrine

--- AccseeToken.php
+++ AccseeToken.php
@@ -17,4 +17,66 @@ class AccessToken
         $this->set('appsecret', $appsecret);
     }
 
+    /**
+     * 获取 AccessToken（调用缓存，返回 String）
+     */
+    public function getTokenString()
+    {
+        $cacheId = $this->getCacheId();
+
+        if( $this->cache && $data = $this->cache->fetch($cacheId) ) {
+            return $data['access_token'];
+        }
+
+        $response = $this->getTokenResponse();
+
+        if( $this->cache ) {
+            $this->cache->save($cacheId, $response, $response['expires_in']);
+        }
+
+        return $response['access_token'];
+    }
+
+    /**
+     * 获取 AccessToken（不缓存，返回原始数据）
+     */
+    public function getTokenResponse()
+    {
+        $query = array(
+            'grant_type'    => 'client_credential',
+            'appid'         => $this['appid'],
+            'secret'        => $this['appsecret']
+        );
+
+        $response = Http::request('GET', static::ACCESS_TOKEN)
+            ->withQuery($query)
+            ->send();
+
+        if( $response->containsKey('errcode') ) {
+            throw new \Exception($response['errmsg'], $response['errcode']);
+        }
+
+        return $response;
+    }
+
+
+    /**
+     * 从缓存中清除
+     */
+    public function clearFromCache()
+    {
+        return $this->cache
+            ? $this->cache->delete($this->getCacheId())
+            : false;
+    }
+
+
+    /**
+     * 获取缓存 ID
+     */
+    public function getCacheId()
+    {
+        return sprintf('%s_access_token', $this['appid']);
+    }
+
 }
